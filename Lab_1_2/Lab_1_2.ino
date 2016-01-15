8dfb5f2884d598c9cf75b7411423b841ab935a40
Quentin Torgerson <quentin.torgerson@gmail.com>
2016-01-15 15:44:54 -0500

    fixed switch debounce in interrupt

--- Lab_1_2/Lab_1_2.ino
+++ Lab_1_2/Lab_1_2.ino
@@ -21,6 +21,9 @@ byte LED_pin_2 = 5;
 byte LED_pin_3 = 6;
 byte LED_pin_4 = 7;
 
+unsigned long last_count_time = 0;
+unsigned long last_reverse_time = 0;
+
 // count and direction variables
 volatile byte button_count = 0;
 volatile int count_direction = 1;
@@ -31,11 +34,12 @@ int debounce_time = 50;
 void setup() {
   // set pin modes for all input and output pins
   // inputs with pullup resistor to pull them high when disconnected from ground
+  Serial.begin(9600);
   pinMode(INPUT, input_pin_1);
   pinMode(INPUT, input_pin_2);
   digitalWrite(input_pin_1, HIGH);
   digitalWrite(input_pin_2, HIGH);
-  
+
   //attach an interrupt to both pins that runs when they are low
   attachInterrupt(digitalPinToInterrupt(input_pin_1), count, LOW);
   attachInterrupt(digitalPinToInterrupt(input_pin_2), rverse, LOW);
@@ -58,23 +62,29 @@ void loop() {
 // the count ISR is run when pin 2 is low
 void count() {
   //debounce code here
-  unsigned long current_time = millis();
-  button_count += count_direction;
-  if (button_count == 16) {
-    button_count = 0;
+  if (millis() - last_count_time > debounce_time) {
+    button_count += count_direction;
+    if (button_count == 16) {
+      button_count = 0;
+    }
+    else if (button_count == 255) {
+      button_count = 15;
+    }
+    Serial.println("countLoop");
   }
-  else if (button_count == 255) {
-    button_count = 15;
-  }
-  while (!digitalRead(input_pin_1) && (millis() - current_time < debounce_time)) {}
+  last_count_time = millis();
+  while (!digitalRead(input_pin_1)) {}
+
 }
 
 
 // the reverse ISR is run when pin 3 is low
 void rverse() {
-  unsigned long current_time = millis();
-  // increment the button_count variable, and control the overflow with an if statement
-  count_direction *= -1;
-  while (!digitalRead(input_pin_2) && (millis() - current_time < debounce_time)) {}
+  if (millis() - last_reverse_time > debounce_time) {
+    // increment the button_count variable, and control the overflow with an if statement
+    count_direction *= -1;
+  }
+  last_reverse_time = millis();
+  while (!digitalRead(input_pin_2)) {}
 }
 
