9b5dd7cb3e6aec45394183ab4ba8f43851edd066
Quentin Torgerson <quentin.torgerson@gmail.com>
2016-01-22 16:37:44 -0500

    Fixed Comments

--- Lab_2_2/Lab_2_2.ino
+++ Lab_2_2/Lab_2_2.ino
@@ -1,6 +1,9 @@
 //////////   Hardware setup  /////////////////////////////////
 
-// pin11 --> 
+// pin10 --> 330 ohm resistor --> transistor base
+// transistor emitter --> ground
+// 5V --> motor --> transistor collector
+// 5V --> diode cathode --> diode anode --> transistor collector 
 
 // pinA0 --> middle potentiometer pin
 // 5V --> outer potentiometer pin 1
@@ -16,6 +19,7 @@ byte sense_pin = A0;
 // test what the possible min and max sensor readings could be and insert
 int min_sensor_value = 0;
 int max_sensor_value = 1023;
+
 int min_motor_speed = 0;
 int max_motor_speed = 255;
 
@@ -23,9 +27,8 @@ byte motor_speed = 0;
 
 void setup() {
 
-  // uncomment the Serial.print lines to print sensor readings.  
-  // Leaving them running will slow the code when running the chaser though
-  Serial.begin(9600);
+  // uncomment the Serial.print lines to print motor speed.  
+  //Serial.begin(9600);
   
   // outputs
   pinMode(motor_pin, OUTPUT);
@@ -38,10 +41,10 @@ void loop() {
   // scale the sensor reading to the min and max delay times using the map() function
   motor_speed = map(analogRead(sense_pin), min_sensor_value, max_sensor_value, min_motor_speed, max_motor_speed);
 
-  // uncomment the Serial.print lines to print sensor readings.  
-  // Leaving them running will slow the code when running the chaser though
-  Serial.print("mtor_speed = ");
-  Serial.println(motor_speed);
+  // uncomment the Serial.print lines to print motor speed.  
+  //Serial.print("mtor_speed = ");
+  //Serial.println(motor_speed);
 
+  // set motor PWM to motor_speed
   analogWrite(motor_pin, motor_speed);
 }
